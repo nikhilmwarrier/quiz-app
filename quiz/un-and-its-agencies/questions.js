740d482a0a53ce2d82996424568b61ffe62c0cd6
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2020-10-24 11:58:56 +0530

    Add UN quiz [10 question]

--- quiz/un-and-its-agencies/questions.js
+++ quiz/un-and-its-agencies/questions.js
@@ -10,12 +10,80 @@ const questions = [
     },
     
     {
-        question: 'Who wrote "The Time Machine"?',
+        question: 'Which of the following are undertaken by UNESCO to promote international cooperation?',
         answers: [
-            { text: 'Washington Irving', correct: false },
-            { text: 'HG Wells', correct: true },
-            { text: 'Jules Verne', correct: false },
+            { text: 'Education', correct: false },
+            { text: 'Cultural Heritage', correct: false },
+            { text: 'Science', correct: false },
+            { text: 'All of these', correct: true },
+        ]
+    },
+    {
+        question: 'Which is the correct expansion of WFP, which provides food and assistance in 83 countries of the world?',
+        answers: [
+            { text: 'World Food Production', correct: false },
+            { text: 'World Feeding Programme', correct: false },
+            { text: 'World Food Programme', correct: true },
             { text: 'None of these', correct: false },
         ]
     },
+        {
+        question: 'When was UN founded?',
+        answers: [
+            { text: 'October 24 1935', correct: false },
+            { text: 'September 24 1945', correct: false },
+            { text: 'October 24 1945', correct: true },
+            { text: 'September 24 1935', correct: false },
+        ]
+    },
+        {
+        question: 'Where are the headquarters of UNESCO?',
+        answers: [
+            { text: 'Paris, France', correct: true },
+            { text: 'New York, USA', correct: false },
+            { text: 'Queensland, Australia', correct: false },
+            { text: 'Hamburg, Germany', correct: false },
+        ]
+    },
+        {
+        question: 'Where was the first session of UN held?',
+        answers: [
+            { text: 'Central Hall, Wesminster, London', correct: true },
+            { text: 'First Avenue 750, New York, USA', correct: false },
+            { text: 'No. 7, Place de Fontenoy, Paris, France', correct: false },
+            { text: 'None of these', correct: false },
+        ]
+    },
+        {
+        question: 'State true or false: "UNESCO has 193 Members and 11 Associate Members"',
+        answers: [
+            { text: 'True', correct: true },
+            { text: 'False', correct: false },
+        ]
+    },
+        {
+        question: 'Where are the headquarters of WHO?',
+        answers: [
+            { text: 'Geneva, Switzerland', correct: true },
+            { text: 'Paris, France', correct: false },
+            { text: 'New York, USA', correct: false },
+            { text: 'None of these', correct: false },
+        ]
+    },
+           {
+        question: 'When was WHO founded?',
+        answers: [
+            { text: '6 April 1947', correct: false },
+            { text: '7 April 1948', correct: true },
+            { text: '6 April 1948', correct: false },
+            { text: '7 April 1947', correct: false },
+        ]
+    },
+               {
+        question: 'State true or false: "WHO supplies vaccines to 50% of the world\'s children"',
+        answers: [
+            { text: 'True', correct: true },
+            { text: 'False', correct: false },
+        ]
+    }
 ]
